857a86daf0fa62c14249b33f66c207b82d9ba1d5
Sameer Sondhi <sameersondhi@outlook.com>
2023-05-18 12:03:17 -0700

    Added a Vehicle component and explored mobile UI

--- client/src/Page/Vehicles.jsx
+++ client/src/Page/Vehicles.jsx
@@ -1,48 +1,8 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
+import Vehicle from "../components/Vehicle";
 
-const Vehicles = () => {
+export default function Vehicles(){
   return (
-    <div>
-        <h1>2023 Toyota Corolla Hybrid</h1>
-        <Paper elevation={11}>
-      <Card sx={{ Width: "100rem" }}>
-        <CardMedia
-          sx={{ height: 300 }}
-          image="https://placekitten.com/g/1800/400"
-          title="Homepage Cover Photo"
-        />
-
-        <CardContent
-          sx={{
-            marginY: 35,
-            height: "80%",
-          }}
-        >
-          <Typography gutterBottom variant="h5" component="div" alignCenter>
-            Grand Tourer
-          </Typography>
-          <Typography variant="body1" color="text.secondary">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
-            consequatur, in dolor eaque fugiat quos eum labore iusto error,
-            autem ut quasi optio quibusdam amet at sequi voluptatibus fugit
-            libero!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Dolorem consequatur, in dolor eaque fugiat quos eum labore iusto
-            error, autem ut quasi optio quibusdam amet at sequi voluptatibus
-            fugit libero!Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Dolorem consequatur, in dolor eaque fugiat quos eum labore
-            iusto error, autem ut quasi optio quibusdam amet at sequi
-            voluptatibus fugit libero!
-          </Typography>
-        </CardContent>
-      </Card>
-    </Paper>
-    </div>
+       <Vehicle/>
   )
 }
-
-export default Vehicles
