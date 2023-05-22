68cd67bd484f0ec71d028955c797355615af8ddc
Harold Barleta <hbf20c@gmail.com>
2023-05-22 10:58:00 -0700

    Update Vehicle.jsx

--- client/src/components/Vehicle.jsx
+++ client/src/components/Vehicle.jsx
@@ -4,46 +4,38 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import corolla from '../static/Pasi-Corolla-Hybrid.jpg'
+import corolla from "../static/Pasi-Corolla-Hybrid.jpg";
 import { vehicles } from "../constants/info";
 
-const VehicleCard=({vehicle})=>(
-<Paper elevation={11}>
-      <Card sx={{ Width: "100rem", backgroundColor:"#272727", color:"gold" }}>
-        <CardMedia
-          sx={{ width:"100vw",height: "100vh" }}
-          image={vehicle.image}
-          title="Homepage Cover Photo"
-        />
+const VehicleCard = ({ vehicle }) => (
+  <Paper elevation={11}>
+    <Card sx={{ Width: "100rem", backgroundColor: "#272727", color: "gold" }}>
+      <CardMedia
+        sx={{ width: "100vw", height: "100vh" }}
+        image={`${vehicle.image}`}
+        title="Homepage Cover Photo"
+      />
 
-        <CardContent
-          sx={{
-            marginY: 35,
-            height: "80%",
-          }}
-        >
-          <Typography gutterBottom variant="h5" component="div" alignCenter>
-            {vehicle.title}
-          </Typography>
-          <Typography variant="body1">
-            {vehicle.description}
-          </Typography>
-        </CardContent>
-      </Card>
-    </Paper> 
-  
-)
-export default function Vehicle(){
+      <CardContent
+        sx={{
+          marginY: 35,
+          height: "80%",
+        }}
+      >
+        <Typography gutterBottom variant="h5" component="div" alignCenter>
+          {vehicle.title}
+        </Typography>
+        <Typography variant="body1">{vehicle.description}</Typography>
+      </CardContent>
+    </Card>
+  </Paper>
+);
+export default function Vehicle() {
   return (
     <>
-    {vehicles.map((vehicle, index)=>(
-      <VehicleCard key={index} vehicle={vehicle}/>
-    
-    )
-    )
-  
-    }
+      {vehicles.map((vehicle, index) => (
+        <VehicleCard key={index} vehicle={vehicle} />
+      ))}
     </>
-  
-  )
+  );
 }
